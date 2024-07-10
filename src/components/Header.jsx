bb7e24e416d07896f66eba2f9484a20955478358
alessiavozzo <a.vozzo95@gmail.com>
2024-07-10 16:36:47 +0200

    add basic header using bs-react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,40 @@
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import './css/Header.css';
+
 function Header() {
     return (
         <header>
-            header
+            {/* orange topbar */}
+            <div className="top-banner">
+                <span>Ottieni il tuo primo corso con uno sconto del 50%. L'offerta è valida solo per i primi 50 studenti. </span>
+                <a href="#" className='register' tabIndex={0}>Registrati adesso</a>
+            </div>
+
+            {/* navbar with offcanvas menu and icons */}
+            <Container>
+                <Navbar expand="lg" className='circus-navbar'>
+                    <Navbar.Brand href="#">
+                        <img src="/img/logo.png" alt="code circus green logo" tabIndex={0} />
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#" tabIndex={0} >Home</Nav.Link>
+                            <NavDropdown title="Corsi" id="courses-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#" tabIndex={0}>Eventi</Nav.Link>
+                            <Nav.Link href="#" tabIndex={0}>Contatti</Nav.Link>
+                            <Nav.Link href="#" tabIndex={0}>Registrati</Nav.Link>
+                        </Nav>
+
+                    </Navbar.Collapse>
+                </Navbar>
+            </Container>
         </header>
     )
 }
