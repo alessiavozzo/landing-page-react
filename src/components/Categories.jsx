cf9a9ac2591c0e05a3f494f31026046c972e2c01
alessiavozzo <a.vozzo95@gmail.com>
2024-07-11 01:06:42 +0200

    add complete category cards

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -18,7 +18,7 @@ function Categories() {
 
     return (
         <section id="categories" className='pt-5'>
-            <Container fluid="xl">
+            <Container fluid="lg">
                 <div className="pick mb-2">
                     Scegli la categoria di corsi dalla quale vuoi iniziare
                 </div>
@@ -29,14 +29,29 @@ function Categories() {
                 </h2>
 
                 {/* see all btn */}
-                <a href="#" className='btn see-all-btn rounded-5' aria-label="Guarda tutte le categorie">Guarda tutto</a>
+                <a href="#" className='btn see-all-btn rounded-5 mb-4' aria-label="Guarda tutte le categorie">Guarda tutto</a>
 
                 <Row className='g-3'>
                     {categoryColumns.map(column => (
-                        <Col className='col-12 col-xl-4 d-flex flex-column flex-xl-row gap-2'>
+                        <Col className='col-12 col-lg-4 d-flex flex-column gap-2'>
                             {column.map(category => (
-                                <Card>
-                                    {category.title}
+                                <Card className='p-4 border-0 d-flex flex-row gap-3 align-items-center' style={{ backgroundColor: category.bgColor }}>
+
+                                    {/* icon */}
+                                    <div className="category-icon" aria-label={`icon of ${category.iconId}`}>
+                                        <svg id={category.iconId} viewBox="0 0 24 24">
+                                            <path fill={category.iconColor}
+                                                d={category.d} />
+                                        </svg>
+                                    </div>
+
+                                    {/* text */}
+                                    <div className="category-text">
+                                        <h6>{category.title}</h6>
+                                        <div className="courses-number">
+                                            {category.courses}
+                                        </div>
+                                    </div>
                                 </Card>
                             ))}
                         </Col>
