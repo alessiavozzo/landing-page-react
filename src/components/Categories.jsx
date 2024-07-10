fbb4603d405a18f700f09c6eaebb74b691e09275
alessiavozzo <a.vozzo95@gmail.com>
2024-07-11 01:27:13 +0200

    add hover effect on category cards

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,6 +2,7 @@ import '../assets/js/categories';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import './css/Categories.css';
 import categories from '../assets/js/categories';
+import { useState } from 'react';
 
 function getColumns(array, columnElements) {
     let result = [];
@@ -16,8 +17,10 @@ function Categories() {
     let categoryColumns = getColumns(categories, 3);
     /*  console.log(categoryColumns); */
 
+    const [hoveredCard, setHoveredCard] = useState(null);
+
     return (
-        <section id="categories" className='pt-5'>
+        <section id="categories" className='py-5'>
             <Container fluid="lg">
                 <div className="pick mb-2">
                     Scegli la categoria di corsi dalla quale vuoi iniziare
@@ -32,21 +35,26 @@ function Categories() {
                 <a href="#" className='btn see-all-btn rounded-5 mb-4' aria-label="Guarda tutte le categorie">Guarda tutto</a>
 
                 <Row className='g-3'>
-                    {categoryColumns.map(column => (
-                        <Col className='col-12 col-lg-4 d-flex flex-column gap-2'>
+                    {categoryColumns.map((column, index) => (
+                        <Col key={index} className='col-12 col-lg-4 d-flex flex-column gap-2'>
                             {column.map(category => (
-                                <Card className='p-4 border-0 d-flex flex-row gap-3 align-items-center' style={{ backgroundColor: category.bgColor }}>
+                                <Card
+                                    key={category.id}
+                                    className='p-4 border-0 d-flex flex-row gap-3 align-items-center'
+                                    onMouseEnter={() => setHoveredCard(category.id)}
+                                    onMouseLeave={() => setHoveredCard(null)}
+                                    style={{ backgroundColor: hoveredCard === category.id ? category.iconColor : category.bgColor }}>
 
                                     {/* icon */}
                                     <div className="category-icon" aria-label={`icon of ${category.iconId}`}>
                                         <svg id={category.iconId} viewBox="0 0 24 24">
-                                            <path fill={category.iconColor}
+                                            <path fill={hoveredCard === category.id ? 'white' : category.iconColor}
                                                 d={category.d} />
                                         </svg>
                                     </div>
 
                                     {/* text */}
-                                    <div className="category-text">
+                                    <div className="category-text" style={{ color: hoveredCard === category.id ? 'white' : 'black' }}>
                                         <h6>{category.title}</h6>
                                         <div className="courses-number">
                                             {category.courses}
