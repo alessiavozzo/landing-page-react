951ec069ae04595733f7d22ae7f26b8fb8682367
alessiavozzo <a.vozzo95@gmail.com>
2024-07-10 19:58:55 +0200

    fix jumbotron aria labels

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -16,10 +16,10 @@ function Jumbotron() {
                             <input
                                 type='search'
                                 placeholder="Cosa vuoi imparare oggi?"
-                                aria-label="Username"
+                                aria-label="Cerca cosa vuoi imparare oggi"
                                 className='form-control input_search rounded-4'
                             />
-                            <button type="submit" className='search-btn d-flex align-items-center justify-content-center rounded-4'>
+                            <button type="submit" className='search-btn d-flex align-items-center justify-content-center rounded-4' aria-label="research button">
                                 <svg id="search" viewBox="0 0 24 24">
                                     <path fill="currentColor"
                                         d="M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.39M11 18a7 7 0 1 1 7-7a7 7 0 0 1-7 7" />
