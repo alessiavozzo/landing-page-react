e978f950168e7837e80a6d8340de8555130d2d94
alessiavozzo <a.vozzo95@gmail.com>
2024-07-12 20:09:34 +0200

    add teachers css

--- src/components/SingleTeacher.jsx
+++ src/components/SingleTeacher.jsx
@@ -4,8 +4,8 @@ import './css/Teachers.css';
 function SingleTeacher({ teacher }) {
     return (
         <Card className='border-0 p-3 rounded-3'>
-            <Card.Img variant="top" src={`/img/${teacher.img}`} className='rounded-3' alt={`${teacher.name} picture`} />
-            <Card.Title>
+            <Card.Img variant="top" src={`/img/${teacher.img}`} className='rounded-3 mb-3' alt={`${teacher.name} picture`} />
+            <Card.Title className='teacher_name mb-1'>
                 {teacher.name}
             </Card.Title>
             <Card.Text className='teacher_job'>
--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -5,13 +5,13 @@ import SingleTeacher from './SingleTeacher';
 
 function Teachers() {
     return (
-        <section id="teachers">
+        <section id="teachers" className='py-5'>
             <Container fluid="xl">
                 <div className="stalk mb-2 text-center">
                     Stalkera i nostri insegnanti
                 </div>
                 <h2 className='text-center mb-4'>I Nostri Insegnanti</h2>
-                <Row className='row-cols-1 row-cols-xl-5'>
+                <Row className='row-cols-1 row-cols-xl-5 gy-4'>
                     {teachers.map(teacher => (
                         <Col key={teacher.id}>
                             <SingleTeacher teacher={teacher} />
