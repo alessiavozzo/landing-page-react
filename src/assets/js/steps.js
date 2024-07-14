fc7e600b7abe8010055ef0a3c476e76bfc0f812a
alessiavozzo <a.vozzo95@gmail.com>
2024-07-14 11:49:28 +0200

    add steps css

--- src/assets/js/steps.js
+++ src/assets/js/steps.js
@@ -3,19 +3,25 @@ const steps = [
         id: 1,
         title: 'Primo Step',
         text: 'Iscriviti al nostro sito e crea il tuo profilo personale. In pochi minuti sarai pronto per esplorare il nostro mondo di corsi di programmazione divertenti e stravaganti. Non aspettare, il viaggio nel mondo del coding comincia ora!',
-        btn: 'Registrati'
+        btn: 'Registrati',
+        iconId: "arrow-circle-right",
+        iconD: "M15.71 12.71a1 1 0 0 0 .21-.33a1 1 0 0 0 0-.76a1 1 0 0 0-.21-.33l-3-3a1 1 0 0 0-1.42 1.42l1.3 1.29H9a1 1 0 0 0 0 2h3.59l-1.3 1.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0ZM22 12a10 10 0 1 0-10 10a10 10 0 0 0 10-10M4 12a8 8 0 1 1 8 8a8 8 0 0 1-8-8"
     },
     {
         id: 2,
         title: 'Segui I Corsi Online',
         text: "Partecipa ai nostri corsi online interattivi, progettati per essere coinvolgenti e pieni di risate. Impara nuove competenze dai nostri istruttori eccentrici e divertiti mentre codifichi, con progetti unici e sfide creative. Ogni lezione è un'avventura!",
-        btn: 'Partecipa'
+        btn: 'Partecipa',
+        iconId: 'Fitness',
+        iconD: "M20.57 14.86L22 13.43L20.57 12L17 15.57L8.43 7L12 3.43L10.57 2L9.14 3.43L7.71 2L5.57 4.14L4.14 2.71L2.71 4.14l1.43 1.43L2 7.71l1.43 1.43L2 10.57L3.43 12L7 8.43L15.57 17L12 20.57L13.43 22l1.43-1.43L16.29 22l2.14-2.14l1.43 1.43l1.43-1.43l-1.43-1.43L22 16.29z"
     },
     {
         id: 3,
         title: 'Ricevi Una Pacca Sulla Spalla',
         text: "Al termine di ogni corso, riceverai il riconoscimento per il tuo impegno e le tue abilità. Condividi i tuoi successi con la nostra comunità globale e goditi una meritata pacca sulla spalla. Sei pronto a mostrare al mondo il tuo talento nel coding!",
-        btn: 'Certificato'
+        btn: 'Certificato',
+        iconId: 'Laptop',
+        iconD: "M4 6h16v10H4m16 2a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4c-1.11 0-2 .89-2 2v10a2 2 0 0 0 2 2H0v2h24v-2z"
     }
 ];
 
--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -12,12 +12,21 @@ function Steps() {
                         <Row key={step.id} className={`step ${index % 2 === 0 ? 'left' : 'right'}`}>
                             <Col xs={12} xl={6} className={`mb-4 step-col ${index % 2 === 0 ? 'left' : 'right'}`}>
                                 <Card className="step-card">
-                                    <Card.Title className='p-3 step-title'>{step.title}</Card.Title>
+                                    <Card.Title className='p-3 step-title'>
+                                        <span>{step.title}</span>
+                                        <div className="speech"></div>
+                                    </Card.Title>
                                     <Card.Body>
                                         <Card.Text className='step-text'>{step.text}</Card.Text>
                                         <a href="#" aria-label={step.btn} className="btn btn-action rounded-5">{step.btn}</a>
                                     </Card.Body>
                                 </Card>
+                                <div className="step-icon">
+                                    <svg id={step.iconId} viewBox="0 0 24 24">
+                                        <path fill="currentColor"
+                                            d={step.iconD} />
+                                    </svg>
+                                </div>
                             </Col>
                         </Row>
                     ))}
